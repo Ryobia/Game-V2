3516b6ab73a1a7dcfc84aa1479c79f70438b1e77
Ryobia <ryobia36@gmail.com>
2021-09-07 15:58:33 -0600

    step one of bringing over the handi game

--- client/src/components/Circle/index.js
+++ client/src/components/Circle/index.js
@@ -1,25 +1,33 @@
 import React, { useState } from "react";
 
 const Circle = () => {
-  return (
-    <div className="circle-div">
-      <svg
-        width="400"
-        height="400"
-        viewBox="0 0 496 496"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          id="Vector"
-          d="M248 0C111 0 0 111 0 248C0 385 111 496 248 496C385 496 496 385 496 248C496 111 385 0 248 0ZM248 448C137.5 448 48 358.5 48 248C48 137.5 137.5 48 248 48C358.5 48 448 137.5 448 248C448 358.5 358.5 448 248 448Z"
-          fill="black"
-        />
-      </svg>
+  let first = function () {};
 
-      <div id="outer-orbit">
-    <div class="outer-orbit-cirlces"></div>
-  </div>
+  return (
+    <div>
+
+      <section className="buttons">
+        <button onClick={first}>First</button>
+        <button>Second</button>
+        <button>Third</button>
+        <button>Fourth</button>
+      </section>
+
+      <div className="circle-div">
+        <div id="outer-orbit" className="first">
+          <div className="outer-orbit-cirlces"></div>
+        </div>
+        <div id="outer-orbit" className="second">
+          <div className="outer-orbit-cirlces"></div>
+        </div>
+        <div id="outer-orbit" className="third">
+          <div className="outer-orbit-cirlces"></div>
+        </div>
+        <div id="outer-orbit" className="fourth">
+          <div className="outer-orbit-cirlces"></div>
+        </div>
+      </div>
+      
     </div>
   );
 };
--- client/src/components/Game/index.js
+++ client/src/components/Game/index.js
@@ -1,10 +1,270 @@
 import React, { useState } from "react";
+import $ from 'jquery';
 import Circle from "../Circle";
+import playerImg from '../../images/handi-capped.png';
+import baddieImg from '../../images/baddie.png';
+
 
 const Game = () => {
+  let player = $('#player');
+  let baddie = $('#baddie');
+  let field = $('#field');
+  let timerEl = $('#timer');
+  let levelEl = $('#level');
+  let startMenu = $('#startMenu');
+  let start = document.getElementById('start');
+  
+  let level = 1;
+  let speed = 10;
+  let baddieSpeed = 10;
+  let playerX = player.offset();
+  let baddieX = baddie.offset();
+  
+  
+  let init = function() {
+      $('#startMenu').toggle();
+      
+  }
+  
+  let startGame = function() {
+      $('#startMenu').toggle();
+      let time = 0;
+      let backgroundJunk = setInterval(function() {
+          playerX = player.offset();
+          baddieX = baddie.offset();
+          baddieGo(playerX, baddieX);
+  
+  
+      
+          if (hitbox(player, baddie)) {
+              alert("He got you");
+              clearInterval(backgroundJunk);
+              clearInterval(timer);
+              resetGame();
+          } else if (time >= 30) {
+              alert('Congrats! you beat this level!');
+              clearInterval(backgroundJunk);
+              clearInterval(timer);
+              resetGame();
+  
+          }
+      
+      }, 100);
+  
+      let timer = setInterval(function() {
+          time += 1;
+          console.log(time);
+          $(timerEl).html(time);
+          }, 1000);
+  
+  
+  };
+  
+  let resetGame = function() {
+      $('#startMenu').toggle();
+      $(player).css({'position': 'absolute', 'left':'0%', 'bottom':'50%'}); //this works everytime
+  
+      $(baddie).css({'position': 'absolute', 'right':'0%', 'bottom':'50%'});//this rarely works and only if the game ends in a certain place
+  
+      
+  
+  
+  
+      // $(player).style
+      // $(baddie).style    add the screen position reset here
+  
+  };
+  
+  
+  
+  
+  init();
+  
+  let hitbox = function (player, baddie) {
+  
+      let playerOffset = player.offset();
+      let playerHeight = player.outerHeight(true);
+      let playerWidth = player.outerWidth(true);
+      let playerTop = playerOffset.top + playerHeight;
+      let playerLeft = playerOffset.left + playerWidth;
+  
+      let baddieOffset = baddie.offset();
+      let baddieHeight = baddie.outerHeight(true);
+      let baddieWidth = baddie.outerWidth(true);
+      let baddieTop = baddieOffset.top + baddieHeight;
+      let baddieLeft = baddieOffset.left + baddieWidth;
+  
+      return !(playerTop < baddieOffset.top || 
+          playerOffset.top > baddieTop || 
+          playerLeft < baddieOffset.left || 
+          playerOffset.left > baddieLeft);
+  };
+  
+  let outOfBounds = function(player, field, direction) {
+  
+      let playerOffset = player.offset();
+      let playerHeight = player.outerHeight(true);
+      let playerWidth = player.outerWidth(true);
+      let playerTop = playerOffset.top + playerHeight;
+      let playerLeft = playerOffset.left + playerWidth;
+  
+      let fieldOffset = field.offset();
+      let fieldHeight = field.outerHeight(true);
+      let fieldWidth = field.outerWidth(true);
+      let fieldTop = fieldOffset.top + fieldHeight;
+      let fieldLeft = fieldOffset.left + fieldWidth;
+  
+      switch (direction) {
+          case 'w':
+              return !(playerOffset.top - speed < fieldOffset.top);
+              break;
+  
+          case 'a': 
+              return !(playerOffset.left - speed < fieldOffset.left);
+              break;
+          
+          case 's':
+              return !(playerTop + speed > fieldTop);
+              break;
+          
+          case 'd':
+              return !(playerLeft + speed > fieldLeft);
+              break;
+      };
+       
+  
+  };
+  
+  
+  
+  
+  let baddieGo = function(playerX, baddieX) {
+  
+  
+     
+      if (baddieX.left < playerX.left) {
+          $(baddie).animate({left: '+=' + baddieSpeed + 'px'}, 10);
+  
+      }
+      if (baddieX.left > playerX.left) {
+          $(baddie).animate({left: '-=' + baddieSpeed + 'px'}, 10);
+  
+      }
+      if (baddieX.top < playerX.top) {
+          $(baddie).animate({bottom: '-=' + baddieSpeed + 'px'}, 10);
+          
+      }
+      if (baddieX.top > playerX.top) {
+          $(baddie).animate({bottom: '+=' + baddieSpeed + 'px'}, 10);
+      }
+  
+  
+  
+  };
+  
+  
+  
+  document.addEventListener("keydown", function(e) {
+      if(e.key === 'w') {
+          
+          if (outOfBounds(player, field, e.key)) {
+  
+              $(player).animate({bottom: '+=' + speed + 'px'}, 10);
+              // player.style.bottom = `${parseInt(player.style.bottom) + 5}px`;
+         
+          }    
+      }
+  });
+  
+  document.addEventListener("keydown", function(e) {
+      if(e.key === 'a') {
+          
+  
+          if (outOfBounds(player, field, e.key)) {
+  
+              
+              $(player).animate({left: '-=' + speed + 'px'}, 10);
+  
+          }
+        
+          // player.style.left = `${parseInt(player.style.left) - 5}px`;
+  
+  
+      }
+  });
+  
+  document.addEventListener("keydown", function(e) {
+      if(e.key === 's') {
+  
+          if (outOfBounds(player, field, e.key)) {
+  
+              $(player).animate({bottom: '-=' + speed + 'px'}, 10);
+              // player.style.bottom = `${parseInt(player.style.bottom) - 5}px`;
+  
+          }
+      }
+  });
+  
+  document.addEventListener("keydown", function(e) {
+      if(e.key === 'd') {
+  
+          if (outOfBounds(player, field, e.key)) {
+  
+              $(player).animate({left: '+=' + speed + 'px'}, 10);
+              // player.style.left = `${parseInt(player.style.left) + 5}px`;
+              
+          }
+  
+      }
+  });
+  
+ 
+   
+ 
+
 
     return <div>
-        <Circle />
+      
+      <section id="home">
+      <div className="modal" id="startMenu" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenteredLabel" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalCenteredLabel">Generic Survival Game</h5>
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                    <h6>Controls</h6>
+                  <ul>
+                      <li>Up: W</li>
+                      <li>Left: A</li>
+                      <li>Down: S</li>
+                      <li>Right: D</li>
+                  </ul>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" id="difficulty" className="btn btn-primary col" data-dismiss="modal">Difficulty</button>
+                  <button onClick={startGame}>Start Game</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          
+
+
+            <div className="field">
+          
+
+              <div id="field" className="field-content">
+                <img id="player" className="player" src={playerImg} alt="handi-capped" height="50px"/>
+                <img id="baddie" className="baddie" src={baddieImg} alt="The-bad-guy" height="50px"/>    
+              </div>
+            </div>
+      </section>
+        
     </div>;
   
   };
